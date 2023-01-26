cb982b58ef9dae0fde079a77b4eeac801bb240bd
EvanNguyen2001 <113669106+EvanNguyen2001@users.noreply.github.com>
2023-01-26 00:10:22 -0800

    commit message

--- components/Card/index.js
+++ components/Card/index.js
@@ -1,17 +1,19 @@
+import styles from '../../styles/Home.module.css'
+
 export default function Card({
-    degree="none",
-    colour="purple",
-    font="0",
-    image="icon/personTwo.png"
+    degree = "none",
+    colour = "purple",
+    font = "0",
+    image = "icon/personTwo.png"
 }) {
-    return(
-        <div style={{backgroundColor: colour, fontSize:font}}>
+    return (
+        <div className={styles.Oct} style={{ backgroundColor: colour, fontSize: font }}>
             {
-                colour==="red" ? <img src={'icon/person.png'} width ='50'/>:
-                colour==="blue" ? <img src={'icon/personThree.png'} width ='50'/>:
-                                    <img src={image} width='50'/>
+                colour === "red" ? <img src={'icon/person.png'} width='50' /> :
+                    colour === "blue" ? <img src={'icon/personThree.png'} width='50' /> :
+                        <img src={image} width='50' />
             }
-            {degree}
+            <div className={styles.Word}>{degree}</div>
         </div>
     )
 }
--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,5 @@
-export default function about(){
-    return(
+export default function About() {
+    return (
         <>About Page</>
     )
 }
